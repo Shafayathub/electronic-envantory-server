a51641418db4fed98fea9773736b01df47077186
Shafayathub <shafayat.ms@gamilcom>
2023-01-23 12:22:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clusterenvantory.w8qznwv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+const run = async () => {
+  try {
+    await client.connect();
+    const productCollection = client.db('envantory').collection('products');
+
+    app.get('/product', async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+};
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Electronic envantory server');
+});
+
+app.listen(port, () => {
+  console.log('Server is running on the port:', port);
+});
